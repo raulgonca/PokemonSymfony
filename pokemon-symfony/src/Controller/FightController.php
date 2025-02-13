02542e65f593f47c4c99fc44d8dd3817b03acc68
Jfranbm04 <juanfranburgos2004@gmail.com>
2025-02-13 23:04:12 +0100

    prueba2

--- pokemon-symfony/src/Controller/FightController.php
+++ pokemon-symfony/src/Controller/FightController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 #[Route('/fight')]
 final class FightController extends AbstractController
 {
